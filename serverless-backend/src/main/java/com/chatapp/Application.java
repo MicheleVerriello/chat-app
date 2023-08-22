3e8181e6f93a216a332f9df2a86fe612c4f5975e
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-08-22 20:14:34 +0200

    lambda function works locally

--- serverless-backend/src/main/java/com/chatapp/Application.java
+++ serverless-backend/src/main/java/com/chatapp/Application.java
@@ -1,15 +1,21 @@
 package com.chatapp;
 
+import com.chatapp.controller.GenericController;
 import com.chatapp.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "com.chatapp.controller")
-@Import({ UserController.class })
+@Import({
+    UserController.class,
+    GenericController.class
+})
+@EnableMongoRepositories
 public class Application {
 
     public static void main(String[] args) {
